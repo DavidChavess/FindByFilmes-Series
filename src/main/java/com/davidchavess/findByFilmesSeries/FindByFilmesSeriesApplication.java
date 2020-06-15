2c427694c3a54ba3b8dd1de3c5e6d327c433c97e
David Chaves <davi.ch.fe@gmail.com>
2020-06-15 10:02:53 -0300

    projeto criado

---
+++ src/main/java/com/davidchavess/findByFilmesSeries/FindByFilmesSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.davidchavess.findByFilmesSeries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindByFilmesSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindByFilmesSeriesApplication.class, args);
+	}
+
+}
